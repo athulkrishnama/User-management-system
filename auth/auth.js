d7e4afaf909d0ebf1cf55a2aeef67425de017f5c
athulkrishnama <athulkrishnama24@gmail.com>
2024-10-11 16:46:31 +0530

    auth middleware added

--- auth/auth.js
+++ auth/auth.js
@@ -1,21 +1,38 @@
 const checkLogin = (req, res, next)=>{
-    if(req.session.username){
+    if(req.session.user?.username){
         res.redirect('/');
+    }else{
+        next();
+    }
+}
+const checkAdminLogin = (req, res, next)=>{
+    if(req.session.admin?.username){
+        res.redirect('/admin');
     }else{
         next();
     }
 }
 
 const checkLogout = (req, res, next)=>{
-    if(req.session.username){
+    if(req.session.user?.email){
         next();
     }else{
         res.redirect('/login');
     }
 
 }
+const checkAdminLogout = (req, res, next)=>{
+    if(req.session.admin?.email){
+        next();
+    }else{
+        res.redirect('/admin/login');
+    }
+
+}
 
 module.exports = {
     checkLogin,
-    checkLogout
+    checkLogout,
+    checkAdminLogin,
+    checkAdminLogout
 }
