a4638d6a896583910508856d89b3050ebd8bd84d
athulkrishnama <athulkrishnama24@gmail.com>
2024-10-13 18:18:47 +0530

    added static folder

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const nocache = require('nocache');
 
 const app = express();
 
+app.use(express.static('public'))
 app.use(nocache())
 // connecting to database
 mongoose.connect("mongodb://127.0.0.1:27017/adminDatabase");
